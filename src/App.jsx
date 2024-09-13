9335497a8840646652c5c8d7c8c658ef8ecfa1f2
GURUPRASADHM2003 <159763738+GURUPRASADHM2003@users.noreply.github.com>
2024-09-13 14:22:53 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,154 +1,17 @@
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import TodoCard from './TodoCard';
+import { Container, Button, Form, Dropdown, Card, Row, Col } from 'react-bootstrap';
+import Card from './assets/card';
 
-import "bootstrap/dist/css/bootstrap.min.css"
-import Card from "./Card"
-import './App.css'
 
-
-function App()
-
-{
-  const cards= [
-    {
-      plan : "FREE" ,
-      price : 0,
-      features : [
-    {
-       name :"Single User",
-       enable : true
-    },
-       {
-        name :"50GB Storage",
-        enable : true
-    },
-    { 
-       name :"Unlimited Public Projects",
-       enable : true
-    },
-    {  
-      name :"Community Access",
-      enable : true
-    },
-       
-    {
-      name :"Unlimited Private Projects",
-      enable : false
-    },
-    {
-       name :"Dedicated Phone Support",
-       enable : false
-    },
-    {
-      name :"Dedicated Phone Support",
-      enable : false
-   },
-    {
-        name :"Free Subdomain",
-        enable : false
-    },
-    {
-       name :"Monthly Status Reports",
-       enable : false
-    }
-      ]
-    
-    },
-    {
-      plan : "PLUS" ,
-      price : 9,
-      features : [
-    {
-       name :"5 Users",
-       enable : true
-    },
-       {
-        name :"50GB Storage",
-        enable : true
-    },
-    { 
-       name :"Unlimited Public Projects",
-       enable : true
-    },
-    {  
-      name :"Community Access",
-      enable : true
-    },
-       
-    {
-      name :"Unlimited Private Projects",
-      enable : true
-      
-    },
-    {
-       name :"Dedicated Phone Support",
-       enable : true
-    },
-    {
-        name :"Free Subdomain",
-        enable : true
-    },
-    {
-       name :"Monthly Status Reports",
-       enable : false
-    }
-      ]
-    
-    },
-    {
-      plan : "PRO" ,
-      price : 49,
-      features : [
-    {
-       name :"Unlimited Users",
-       enable : true
-    },
-       {
-        name :"50GB Storage",
-        enable : true
-    },
-    { 
-       name :"Unlimited Public Projects",
-       enable : true
-    },
-    {  
-      name :"Community Access",
-      enable : true
-    },
-       
-    {
-      name :"Unlimited Private Projects",
-      enable : true 
-    },
-    {
-       name :"Dedicated Phone Support",
-       enable : true
-    },
-    {
-        name :"Free Subdomain",
-        enable : true
-    },
-    {
-       name :"Monthly Status Reports",
-       enable : true
-    }
-      ]
-    
-    }
-  ];
+function App() {
+  
   return (
-    <div className="container">
-
-      <main>
-        <div className="row row-cols-1 row-cols-md-3 mb-3 text-center">
-         {
-          cards.map((card,index) =>{
-            return <Card key={index} card={card}/>
-          })
-         }
-        </div>
-      </main>
-    </div>
-  )
-
+    <Container>
+     <Card></Card>
+    </Container>
+  );
 }
 
-export default App
+export default App;
